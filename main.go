cb8c1d4fc7c17102bc86eeba30e3c58fff0fce6b
satyammmmmmm <satyamgupta40@gmail.com>
2023-09-29 14:46:44 +0530

    new11gh1

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import (
 
 	"satyam/decrpt"
 	"satyam/encypt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	_ := gin.New()
 	fmt.Println(encypt.Nimbus("satyam"))
 	fmt.Println(decrpt.Nimbus())
 }
